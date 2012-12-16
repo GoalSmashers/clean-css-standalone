c07eae51ff51a64b0e8331559acfa4571a0a6314
Jakub Pawlowicz <jakub@goalsmashers.com>
2012-12-16 11:42:48 +0000

    Added quotation stripping, squeezing repeating padding/margin/border declaration - on par with clean-css 0.9.0.

--- test/binary-test.js
+++ test/binary-test.js
@@ -49,7 +49,7 @@ exports.commandsSuite = vows.describe('binary commands').addBatch({
   }),
   'version': binaryContext('-v', {
     'should output help': function(error, stdout) {
-      assert.equal(stdout, "0.8.3\n");
+      assert.equal(stdout, "0.9.0\n");
     }
   }),
   'stdin': pipedContext("a{color: #f00}", '', {
