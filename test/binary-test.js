070ee355e9591f2fb199522e0eef326ddc4890ce
Jakub Pawlowicz <jakub@goalsmashers.com>
2013-10-28 22:33:24 +0100

    On par with clean-css 1.1.7.
    * Fixes `@import` processing inside comments.

--- test/binary-test.js
+++ test/binary-test.js
@@ -49,7 +49,7 @@ exports.commandsSuite = vows.describe('binary commands').addBatch({
   }),
   'version': binaryContext('-v', {
     'should output help': function(error, stdout) {
-      assert.equal(stdout, "1.1.6\n");
+      assert.equal(stdout, "1.1.7\n");
     }
   }),
   'stdin': pipedContext("a{color: #f00}", '', {
